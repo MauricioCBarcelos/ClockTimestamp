a21551f2cecb7f3609f015dd8cc19f3665759406
MauricioCBarcelos <43964618+MauricioCBarcelos@users.noreply.github.com>
2019-07-11 11:14:50 -0300

    v1

---
+++ src/main/java/zaas/ClockTimestamp/controller/ControllerTimer.java
@@ -0,0 +1,15 @@
+package zaas.ClockTimestamp.controller;
+
+public class ControllerTimer {
+
+	public String timerControllerNormal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String timerControllerTimestamp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
